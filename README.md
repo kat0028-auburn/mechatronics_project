d0c97a51c117c33c451acfca1a555fe40c9b3a7e
Kyle Thompson <kylethompson27@outlook.com>
2025-04-24 15:43:20 -0500

    simple_solver

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mechatronics_project
 Steps in 90 degree turn - 1190
+
+Stepper Motor Modes: 
+0 - Stopped 
+1 - Forward
+2 - Backwards
+3 - Turn (right/left)
+4 - Turn (right/left)
--- main_ws/src/maze_solver/src/maze_solver_node.cpp
+++ main_ws/src/maze_solver/src/maze_solver_node.cpp
@@ -13,6 +13,8 @@ class MazeSolverNode
     ros::Subscriber sonar_sub;
     ros::Publisher motor_pub;
 
+    int turn_steps;
+
     void sonarCallback(const hardware_serial_interface::SonarArray::ConstPtr &msg);
 };
 
@@ -25,6 +27,7 @@ MazeSolverNode::MazeSolverNode()
 
     this->n.param<std::string>("sonar_topic", sonar_topic, "sonar");
     this->n.param<std::string>("motor_topic", motor_topic, "motor");
+    this->n.param<int>("turn_steps", turn_steps, 1190);
 
     this->sonar_sub = this->n.subscribe(sonar_topic, 1, &MazeSolverNode::sonarCallback, this);
     this->motor_pub = this->n.advertise<hardware_serial_interface::StepperArray>(motor_topic, 1);
@@ -40,42 +43,16 @@ void MazeSolverNode::sonarCallback(const hardware_serial_interface::SonarArray::
     std::cout << msg->sonar_front << ", " << msg->sonar_left << ", " << msg->sonar_right << std::endl;
     hardware_serial_interface::StepperArray stepper_msg;
 
-    if (msg->sonar_left > 20)
-    {
-        std::cout << "left-clear, ";
-        stepper_msg.header.stamp = ros::Time::now();
-        stepper_msg.mode = 3;
-        stepper_msg.steps = 200;
-    }
-    else
-    {
-        std::cout << "left-blocked, ";
-    }
-
-    if (msg->sonar_right > 20)
-    {
-        std::cout << "right-clear" << std::endl;
-        stepper_msg.header.stamp = ros::Time::now();
-        stepper_msg.mode = 4;
-        stepper_msg.steps = 200;
-    }
-    else 
-    {
-        std::cout << "right-blocked" << std::endl;
-    }
-
     if (msg->sonar_front > 20)
     {
-        stepper_msg.header.stamp = ros::Time::now();
         stepper_msg.mode = 1;
-        stepper_msg.steps = 20;    
+        stepper_msg.steps = 20;
     }
-    // else
-    // {
-    //     stepper_msg.header.stamp = ros::Time::now();
-    //     stepper_msg.mode = 0;
-    //     stepper_msg.steps = 0;
-    // }
+    if (msg->sonar_left > 20)
+    {
+        stepper_msg.mode = 2;
+        stepper_msg.steps = turn_steps;
+    }
 
     motor_pub.publish(stepper_msg);
 }
