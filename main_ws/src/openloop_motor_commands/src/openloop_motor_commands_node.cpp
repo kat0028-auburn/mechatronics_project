f3ca2f04d3b92423cce25f4c56ffcbe6c2c14e70
Kyle Thompson <kylethompson27@outlook.com>
2025-04-26 15:21:57 -0500

    update

--- main_ws/src/openloop_motor_commands/src/openloop_motor_commands_node.cpp
+++ main_ws/src/openloop_motor_commands/src/openloop_motor_commands_node.cpp
@@ -87,7 +87,7 @@ int main(int argc, char **argv)
     node.publishMessage(msg);*/
     hardware_serial_interface::StepperArray msg;
     msg.mode = 3;
-    msg.steps = 80;
+    msg.steps = 1151 * 8;
 
     node.publishMessage(msg);
 
